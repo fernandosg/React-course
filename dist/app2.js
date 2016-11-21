08969d8d620f4147bf634b39898ddd81e20e3d85
fsgom <fernandosegom@gmail.com>
2016-11-20 18:36:59 -0600

    Dynamic props in reactjs,now the comments are dynamic created by an array of comments.

--- dist/app2.js
+++ dist/app2.js
@@ -20451,8 +20451,29 @@ var CommentBox = function (_React$Component) {
   }
 
   _createClass(CommentBox, [{
+    key: '_getComments',
+    value: function _getComments() {
+      var commentList = [{ id: 1, author: "Morgan McCircuit from array", body: "Great picture!" }, { id: 2, author: "Bending Bender", body: "Excellent stuff" }];
+
+      return commentList.map(function (comment) {
+        return React.createElement(Comment, { author: comment.author, body: comment.body, key: comment.id });
+      });
+    }
+  }, {
+    key: '_getCommentsTitle',
+    value: function _getCommentsTitle(commentCount) {
+      if (commentCount == 0) {
+        return "Not comment yet";
+      } else if (commentCount == 1) {
+        return "1 comment";
+      } else {
+        return commentCount + ' comments';
+      }
+    }
+  }, {
     key: 'render',
     value: function render() {
+      var comments = this._getComments();
       return React.createElement(
         'div',
         { className: 'comment-box' },
@@ -20464,13 +20485,12 @@ var CommentBox = function (_React$Component) {
         React.createElement(
           'h4',
           { className: 'comment-count' },
-          '2 comments'
+          this._getCommentsTitle(comments.length)
         ),
         React.createElement(
           'div',
           { className: 'comment-list' },
-          React.createElement(Comment, { author: 'Morgan McCircuit', body: 'Great picture!' }),
-          React.createElement(Comment, { author: 'Bending Bender', body: 'Excellent stuff' })
+          comments
         )
       );
     }
--- src/app2.js
+++ src/app2.js
@@ -1,14 +1,33 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
 class CommentBox extends React.Component{
+  _getComments(){
+    const commentList=[
+      {id:1,author:"Morgan McCircuit from array",body:"Great picture!"},
+      {id:2,author:"Bending Bender",body:"Excellent stuff"}
+    ];
+
+    return commentList.map((comment)=>{
+        return (<Comment author={comment.author} body={comment.body} key={comment.id}/>)
+    })
+  }
+  _getCommentsTitle(commentCount){
+    if(commentCount==0){
+      return "Not comment yet";
+    }else if(commentCount==1){
+      return "1 comment";
+    }else{
+      return commentCount+' comments'
+    }
+  }
   render(){
+    const comments=this._getComments();
     return(
       <div className="comment-box">
         <h3>Comments</h3>
-        <h4 className="comment-count">2 comments</h4>
+        <h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
         <div className="comment-list">
-          <Comment author="Morgan McCircuit" body="Great picture!"/>
-          <Comment author="Bending Bender" body="Excellent stuff"/>
+          {comments}
         </div>
       </div>
     )
